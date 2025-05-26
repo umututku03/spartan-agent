ed7206a76206f5ab356ef92b4fb953135ab4aefc
Odilitime <janesmith@airmail.cc>
2025-05-26 18:46:37 +0000

    moving files over

---
+++ src/plugins/degenIntel/interfaces/int_users.ts
@@ -0,0 +1,10 @@
+import type { Plugin } from '@elizaos/core';
+
+// look up by Ids
+
+// list
+function interface_list_users(runtime) {
+
+}
+
+// add/update/delete
---
+++ src/plugins/degenIntel/interfaces/int_wallets.ts
@@ -0,0 +1,17 @@
+import type { Plugin } from '@elizaos/core';
+
+// look up by Ids
+
+// list
+
+// list metawallets by userId
+
+// add/update/delete
+
+
+
+// look up wallet by Ids
+
+// list wallet for metawallet
+
+// add/update/delete wallet
