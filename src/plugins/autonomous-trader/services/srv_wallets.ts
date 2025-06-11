86af2f90bc6cc2b25c9a9e0ed1e5bbe39d470648
Odilitime <janesmith@airmail.cc>
2025-06-11 02:29:42 +0000

    new services

---
+++ src/plugins/autonomous-trader/services/srv_wallets.ts
@@ -0,0 +1,86 @@
+import { IAgentRuntime, getSalt, encryptStringValue, Service, logger } from '@elizaos/core';
+import { acquireService } from '../utils';
+
+import { getWalletByUserEntityIds } from '../interfaces/int_wallets';
+
+export class InterfaceWalletService extends Service {
+  private isRunning = false;
+  private registry: Record<number, any> = {};
+
+  static serviceType = 'AUTONOMOUS_TRADER_INTERFACE_WALLETS';
+  capabilityDescription = 'The agent serves multiple wallets';
+
+  // config (key/string)
+
+  constructor(public runtime: IAgentRuntime) {
+    super(runtime); // sets this.runtime
+    logger.log('AUTONOMOUS_TRADER_INTERFACE_WALLETS constructor');
+  }
+
+  async getWalletByUserEntityIds(entities) {
+    const metawallets = await getWalletByUserEntityIds(this.runtime, entities)
+    return metawallets
+  }
+
+  /**
+   * Start the scenario service with the given runtime.
+   * @param {IAgentRuntime} runtime - The agent runtime
+   * @returns {Promise<ScenarioService>} - The started scenario service
+   */
+  static async start(runtime: IAgentRuntime) {
+    const service = new InterfaceWalletService(runtime);
+    service.start();
+    return service;
+  }
+  /**
+   * Stops the Scenario service associated with the given runtime.
+   *
+   * @param {IAgentRuntime} runtime The runtime to stop the service for.
+   * @throws {Error} When the Scenario service is not found.
+   */
+  static async stop(runtime: IAgentRuntime) {
+    const service = runtime.getService(this.serviceType);
+    if (!service) {
+      throw new Error(this.serviceType + ' service not found');
+    }
+    service.stop();
+  }
+
+  async start(): Promise<void> {
+    if (this.isRunning) {
+      logger.warn('Trading chain service is already running');
+      return;
+    }
+
+    try {
+      logger.info('Starting chain trading service...');
+
+      this.isRunning = true;
+      logger.info('Trading chain service started successfully');
+    } catch (error) {
+      logger.error('Error starting trading chain service:', error);
+      throw error;
+    }
+  }
+
+  async stop(): Promise<void> {
+    if (!this.isRunning) {
+      logger.warn('Trading service is not running');
+      return;
+    }
+
+    try {
+      logger.info('Stopping chain trading service...');
+
+      this.isRunning = false;
+      logger.info('Trading service stopped successfully');
+    } catch (error) {
+      logger.error('Error stopping trading service:', error);
+      throw error;
+    }
+  }
+
+  isServiceRunning(): boolean {
+    return this.isRunning;
+  }
+}
