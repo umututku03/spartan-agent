31811f6ea7eed48ad8e655957eba6ac959a2aae4
Odilitime <janesmith@airmail.cc>
2025-06-26 23:19:34 +0000

    EMAIL_TYPE=>COMPONENT_USER_*, useCodeLength, DESCONLYCALLME

--- src/plugins/autonomous-trader/constants.ts
+++ src/plugins/autonomous-trader/constants.ts
@@ -1,10 +1,19 @@
 
 export const SOLANA_SERVICE_NAME = 'chain_solana';
 
-export const EMAIL_TYPE = 'trader_email_v0'
+export const COMPONENT_USER_TYPE = 'component_user_v0'
+export const COMPONENT_ACCOUNT_TYPE = 'component_account_v0'
 export const SPARTAN_SERVICE_TYPE = 'spartan_services'
 
+export const useCodeLength = 6
+
+export const DESCONLYCALLME = ' ❗️Do NOT include any other action (e.g., REPLY) when calling this. It must be used alone. This action already implies a response.'
+
 export default {
-  EMAIL_TYPE,
+  SOLANA_SERVICE_NAME,
+  COMPONENT_USER_TYPE,
+  COMPONENT_ACCOUNT_TYPE,
   SPARTAN_SERVICE_TYPE,
+  useCodeLength,
+  DESCONLYCALLME,
 }
