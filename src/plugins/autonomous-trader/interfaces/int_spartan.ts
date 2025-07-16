349573e4317ffee02b9512d5eec7935ef3ee2a8d
Odilitime <janesmith@airmail.cc>
2025-07-16 01:39:16 +0000

    strong typing, note

--- src/plugins/autonomous-trader/interfaces/int_spartan.ts
+++ src/plugins/autonomous-trader/interfaces/int_spartan.ts
@@ -1,17 +1,16 @@
 import {
   type IAgentRuntime,
-  ModelType,
-  logger,
-  parseJSONObjectFromText,
+  type Entity,
+  //type Component,
   createUniqueUuid,
 } from '@elizaos/core';
 
 import CONSTANTS from '../constants'
 
 // get by id
-export async function interface_spartan_get(runtime) {
+export async function interface_spartan_get(runtime: IAgentRuntime): Promise<any> {
   const agentEntityId = createUniqueUuid(runtime, runtime.agentId);
-  const agentEntity = await runtime.getEntityById(agentEntityId);
+  const agentEntity = await runtime.getEntityById(agentEntityId) as Entity;
   //console.log('agentEntity', agentEntity)
   let spartanData = agentEntity.components.find(c => c.type === CONSTANTS.SPARTAN_SERVICE_TYPE)
   //let spartanDataNew = false
@@ -19,6 +18,7 @@ export async function interface_spartan_get(runtime) {
   if (!spartanData) {
     // initialize
     //spartanDataNew = true
+    // id, entityId, agentId, roomId
     spartanData = {
       data: {
         users: [],
