ba8c7dc7260b458797fd9be6a98190b5a5f53e37
Odilitime <janesmith@airmail.cc>
2025-07-16 01:37:57 +0000

    stronger typing

--- src/plugins/autonomous-trader/interfaces/int_accounts.ts
+++ src/plugins/autonomous-trader/interfaces/int_accounts.ts
@@ -1,9 +1,8 @@
 import {
   type IAgentRuntime,
-  ModelType,
-  logger,
-  parseJSONObjectFromText,
   type UUID,
+  type Content,
+  type Component,
   createUniqueUuid,
 } from '@elizaos/core';
 import { v4 as uuidv4 } from 'uuid';
@@ -12,7 +11,7 @@ import { interface_users_list, interface_users_ByIds } from './int_users'
 import CONSTANTS from '../constants'
 
 // look up by Ids
-export async function interface_accounts_ByIds(runtime, ids) {
+export async function interface_accounts_ByIds(runtime: IAgentRuntime, ids: UUID[]): Promise<Record<UUID, any>> {
   //console.log('interface_accounts_ByIds', ids)
   const entities = await runtime.getEntityByIds(ids)
 
@@ -46,33 +45,34 @@ export async function interface_accounts_ByIds(runtime, ids) {
   return components
 }
 
-export async function interface_accounts_ByUserIds(runtime, userIds) {
+export async function interface_accounts_ByUserIds(runtime: IAgentRuntime, userIds: UUID[]): Promise<Record<UUID, any>> {
   //console.log('interface_accounts_ByIds', ids)
   const users = await interface_users_ByIds(runtime, userIds)
-  const accountIds = {}
+  // a unique list of users
+  const accountIds = {} // and their single account
   for(const entityId in users) {
     const email = users[entityId]
     console.log(entityId, 'wallets', email.metawallets)
     if (email.verified && email.address) {
-      const emailEntityId = createUniqueUuid(runtime, email.address);
+      const emailEntityId: UUID = createUniqueUuid(runtime, email.address);
       accountIds[entityId] = emailEntityId
       //userWallets[entityId] = email.metawallets
     }
   }
-  const accounts = await interface_accounts_ByIds(runtime, accountIds)
+  const accounts = await interface_accounts_ByIds(runtime, Object.values(accountIds))
   return accounts
 }
 
 // list/search
-export async function interface_accounts_list(runtime, options = {}) {
+export async function interface_accounts_list(runtime: IAgentRuntime, options = {}): Promise<UUID[]> {
   const spartanData = await interface_spartan_get(runtime)
   return spartanData.data.accounts
 }
 
 // list of IDs vs list of users?
-export async function getAccountIdsByPubkeys(runtime, pubkeys) {
-  const mws = []
+export async function getAccountIdsByPubkeys(runtime: IAgentRuntime, pubkeys: string[]): Promise<Record<string, UUID>> {
   /*
+  const mws = []
   const accountIds = await interface_accounts_list(runtime)
   console.log('getAccountIdsByPubkeys - accountIds', accountIds)
   const accounts = await interface_accounts_ByIds(runtime, accountIds)
@@ -164,7 +164,7 @@ export async function getAccountIdsByPubkeys(runtime, pubkeys) {
         console.log('getAccountIdsByPubkeys - no component for', CONSTANTS.COMPONENT_ACCOUNT_TYPE, 'for', accountId)
       }
     } else {
-      console.log('getAccountIdsByPubkeys - no account', accountId, '? weird')
+      console.log('getAccountIdsByPubkeys - no account', account, '? weird')
     }
   }
 
@@ -173,7 +173,7 @@ export async function getAccountIdsByPubkeys(runtime, pubkeys) {
 
 // add/update/delete
 
-export async function interface_account_upsert(runtime, message, account) {
+export async function interface_account_upsert(runtime: IAgentRuntime, message: Content, account): Promise<void> {
   if (account.componentId) {
     console.debug('interface_account_upsert - detected a componentId, weird!', account)
   }
@@ -186,16 +186,16 @@ export async function interface_account_upsert(runtime, message, account) {
   }
 }
 
-export async function interface_account_create(runtime, message, account) {
-  const roomDetails = await runtime.getRoom(message.roomId);
-  const entityId = account.accountEntityId
+export async function interface_account_create(runtime: IAgentRuntime, message: Content, account): Promise<void> {
+  const roomDetails = await runtime.getRoom(message.roomId as UUID);
+  const entityId: UUID = account.accountEntityId
   //console.log('entityId', entityId)
   // create the EMAILTYPE component
   await runtime.createComponent({
     id: uuidv4() as UUID,
-    agentId: runtime.agentId,
-    worldId: roomDetails.worldId,
-    roomId: message.roomId,
+    agentId: runtime.agentId as UUID,
+    worldId: roomDetails.worldId as UUID,
+    roomId: message.roomId as UUID,
     sourceEntityId: message.entityId,
     entityId,
     type: CONSTANTS.COMPONENT_ACCOUNT_TYPE,
@@ -204,7 +204,7 @@ export async function interface_account_create(runtime, message, account) {
 }
 
 // we're not update the account, we're really updating the account one & only component
-export async function interface_account_update(runtime, component) {
+export async function interface_account_update(runtime: IAgentRuntime, component: Component): Promise<boolean> {
   const id = component.id
   if (!id) {
     console.warn('no componentId in account', component)
@@ -231,7 +231,9 @@ export async function interface_account_update(runtime, component) {
   // is just too much
   console.log('interface_account_update - accountId', component.entityId, 'component', component.id)
 
-  const res = await runtime.updateComponent({
+  // const res =
+  // missing: entityId, roomId, worldId, sourceEntityId, createdAt
+  await runtime.updateComponent({
     id: component.id,
     //worldId: roomDetails.worldId,
     //roomId: message.roomId,
