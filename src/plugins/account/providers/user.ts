18c8fe6d427a90565b11604dc421e38015242ec6
Odilitime <janesmith@airmail.cc>
2025-07-25 02:59:25 +0000

    finish implementation

--- src/plugins/account/providers/user.ts
+++ src/plugins/account/providers/user.ts
@@ -1,5 +1,5 @@
 import type { IAgentRuntime, Memory, Provider, State } from '@elizaos/core';
-import { getAccountFromMessage } from '../../autonomous-trader/utils'
+import { getDataFromMessage } from '../../autonomous-trader/utils'
 import { parseDateFilterFromMessage, applyDateFilterToAccount, formatDateFilterText } from '../../autonomous-trader/providers/date_filter'
 
 /**
@@ -8,7 +8,7 @@ import { parseDateFilterFromMessage, applyDateFilterToAccount, formatDateFilterT
  */
 export const userProvider: Provider = {
     name: 'USER_DETAILS',
-    description: 'User-level information',
+    description: 'User registration information like email address and if theyre verified and ready to use services',
     dynamic: true,
     get: async (runtime: IAgentRuntime, message: Memory, state: State) => {
         console.log('USER_DETAILS')
@@ -17,14 +17,25 @@ export const userProvider: Provider = {
 
         // DM or public?
         const isDM = message.content.channelType?.toUpperCase() === 'DM'
+        const componentData = await getDataFromMessage(runtime, message)
         if (isDM) {
           // do we have an registration
           // email set
           // is it registered
-          const component = await getDataFromMessage(runtime, message)
-          console.log('USER_DETAILS component', component)
+          //console.log('USER_DETAILS component', componentData)
+          // address, verified
+          if (componentData.address) {
+            userStr += 'registered with email address: ' + componentData.address + '\n'
+            userStr += 'email address verified: ' + (componentData.verified ? 'verified' : 'awaiting verification') + '\n'
+          } else {
+            userStr += 'user has not registered' + '\n'
+          }
         } else {
-            userStr = 'User details are only available in private messages.'
+          if (componentData.address && componentData.verified) {
+            userStr += 'user is registered' + '\n'
+          } else {
+            userStr += 'user has not registered' + '\n'
+          }
         }
         console.log('userStr', userStr)
 
