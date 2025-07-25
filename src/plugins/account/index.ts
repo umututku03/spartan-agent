22243ccb5095ea3edb37bd1fd22e92f59107619f
Odilitime <janesmith@airmail.cc>
2025-07-25 01:38:39 +0000

    further cleanup, add user provider

--- src/plugins/account/index.ts
+++ src/plugins/account/index.ts
@@ -2,11 +2,11 @@ import type { Plugin } from '@elizaos/core';
 
 // actions
 import { userRegistration } from "./actions/act_reg_start";
-import { checkRegistration } from "./actions/act_reg_query";
+//import { checkRegistration } from "./actions/act_reg_query";
 import { checkRegistrationCode } from "./actions/act_reg_confirmemail";
 import { deleteRegistration } from "./actions/act_reg_delete";
 
-import actEmailUuid from "./actions/act_email_uuid";
+//import actEmailUuid from "./actions/act_email_uuid";
 
 import turnOnNotificationsAction from "./actions/act_turnon_notifications";
 import turnOffNotificationsAction from "./actions/act_turnoff_notifications";
@@ -14,26 +14,22 @@ import turnOffNotificationsAction from "./actions/act_turnoff_notifications";
 // Providers
 
 import { accountProvider } from "./providers/account";
+import { userProvider } from "./providers/user";
 
 // Services
 import { InterfaceUserService } from './services/srv_users';
 import { InterfaceAccountService } from './services/srv_accounts';
 
-function escapeMdV2(text) {
-  return text.replace(/[_*\[\]()~`>#+\-=|{}.!\\]/g, '\\$&');
-}
-
 export const accountRegPlugin: Plugin = {
   name: 'account registration',
   description: 'Register accounts with elizaOS',
   evaluators: [],
-  providers: [multiwalletProvider, walletProvider, accountProvider, tokenProvider],
+  providers: [accountProvider, userProvider],
   actions: [
-    userRegistration, checkRegistrationCode, checkRegistration, deleteRegistration,
+    userRegistration, checkRegistrationCode, deleteRegistration,
     //devFix,
     //actEmailUuid,
     turnOnNotificationsAction, turnOffNotificationsAction,
-    spartanNews
   ],
   services: [InterfaceUserService, InterfaceAccountService],
 };
