10c785d4b0ad129f2d2b0ae3c0436be6cccb1f4a
Odilitime <janesmith@airmail.cc>
2025-07-25 01:39:50 +0000

    getAccountIdsByPubkeys()

--- src/plugins/account/services/srv_accounts.ts
+++ src/plugins/account/services/srv_accounts.ts
@@ -1,6 +1,5 @@
 import { IAgentRuntime, getSalt, encryptStringValue, Service, logger } from '@elizaos/core';
-import { acquireService } from '../utils';
-import { interface_accounts_list, interface_accounts_ByIds, interface_account_update, interface_account_upsert } from '../interfaces/int_accounts'
+import { interface_accounts_list, interface_accounts_ByIds, getAccountIdsByPubkeys, interface_account_update, interface_account_upsert } from '../interfaces/int_accounts'
 import { getUseridsByAccountId } from '../interfaces/int_users'
 
 export class InterfaceAccountService extends Service {
@@ -34,6 +33,10 @@ export class InterfaceAccountService extends Service {
 
   async interface_account_update(componentData) {
     return interface_account_update(this.runtime, componentData)
+  }
+
+  async getAccountIdsByPubkeys(pubkeys: string[]) {
+    return getAccountIdsByPubkeys(this.runtime, pubkeys)
   }
 
   async notifyAccount(accountIds, msg) {
