5ddd64e04256b489b7980cf660861df8fed0f9b2
Odilitime <janesmith@airmail.cc>
2025-07-25 01:44:49 +0000

    split out account/users and other clean up

--- src/plugins/multiwallet/index.ts
+++ src/plugins/multiwallet/index.ts
@@ -1,20 +1,19 @@
 import type { Plugin } from '@elizaos/core';
 
 // actions
-import { userRegistration } from "./actions/act_reg_start";
-import { checkRegistration } from "./actions/act_reg_query";
-import { checkRegistrationCode } from "./actions/act_reg_confirmemail";
-import { deleteRegistration } from "./actions/act_reg_delete";
 
+// move to spartan
+/*
 import { servicesMenu } from "./actions/act_menu";
-
 import { verifyHolder } from "./actions/act_holder_verify";
 import { actHolderQuery } from "./actions/act_holder_query";
-import actEmailUuid from "./actions/act_email_uuid";
-
 import { actionFrequentlyAsked } from "./actions/act_faq";
 import { actionLinks } from "./actions/act_links";
+import { devFix } from "./actions/devfix";
+import spartanNews from "./actions/act_spartan_news";
+*/
 
+// keep
 import { walletCreate } from "./actions/act_wallet_create";
 import { userMetawalletDelete } from "./actions/act_wallet_delete";
 
@@ -24,59 +23,44 @@ import userMetawalletSwap from "./actions/act_wallet_swap";
 import userMetawalletSweep from "./actions/act_wallet_sweep";
 import userMetawalletXfer from "./actions/act_wallet_xfer";
 import userMetawalletBalance from "./actions/act_wallet_balance";
-import userMetawalletPnl from "./actions/act_wallet_pnl";
 
 import userMetawalletSwapAll from "./actions/act_wallet_swap_all";
 
-import actionPositionList from "./actions/act_position_list";
-import openPositionAction from "./actions/act_open_position";
+//import actionPositionList from "./actions/act_position_list";
+//import openPositionAction from "./actions/act_open_position";
 
 //import actionTokenScam from "./actions/act_token_scam";
 //import actionTokenRug from "./actions/act_token_rug";
-
-import turnOnNotificationsAction from "./actions/act_turnon_notifications";
-import turnOffNotificationsAction from "./actions/act_turnoff_notifications";
-
-import { devFix } from "./actions/devfix";
 
 // Providers
 
 import { multiwalletProvider } from "./providers/multiwallet";
 import { walletProvider } from "./providers/wallet";
-import { accountProvider } from "./providers/account";
 import { tokenProvider } from "./providers/token";
 //import { analyticsProvider } from "./providers/analytics";
 
 // Services
-import { InterfaceUserService } from './services/srv_users';
-import { InterfaceAccountService } from './services/srv_accounts';
 import { InterfaceWalletService } from './services/srv_wallets';
-
-function escapeMdV2(text) {
-  return text.replace(/[_*\[\]()~`>#+\-=|{}.!\\]/g, '\\$&');
-}
 
 export const multiwalletPlugin: Plugin = {
   name: 'multitenant wallet',
   description: 'Enduser wallet plugin',
   evaluators: [],
-  providers: [multiwalletProvider, walletProvider, accountProvider, tokenProvider],
+  providers: [multiwalletProvider, walletProvider, tokenProvider],
   actions: [
-    userRegistration, checkRegistrationCode, checkRegistration, deleteRegistration,
-    actionFrequentlyAsked, actionLinks, servicesMenu,
+    //actionFrequentlyAsked, actionLinks, servicesMenu,
     //devFix,
-    verifyHolder, actHolderQuery,
-    //actEmailUuid,
+    //verifyHolder, actHolderQuery,
     walletCreate, walletImportAction, userMetawalletDelete,
     userMetawalletXfer, userMetawalletSwap, userMetawalletSweep, userMetawalletSwapAll,
     userMetawalletList,
-    //userMetawalletBalance, userMetawalletPnl,
+    userMetawalletBalance,
     //actionPositionList,
     //openPositionAction,
     //actionTokenScam, actionTokenRug,
-    turnOnNotificationsAction, turnOffNotificationsAction,
+    //spartanNews
   ],
-  services: [InterfaceUserService, InterfaceAccountService, InterfaceWalletService],
+  services: [InterfaceWalletService],
 };
 
 export default multiwalletPlugin;
