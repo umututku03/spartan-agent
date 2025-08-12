3e03650fe038ab7c29cdf6a2c01a6d6953d045ce
Odilitime <janesmith@airmail.cc>
2025-08-12 19:53:41 +0000

    fix logging labels, getEntityByIds => getEntitiesByIds

--- src/plugins/account/interfaces/int_users.ts
+++ src/plugins/account/interfaces/int_users.ts
@@ -17,7 +17,7 @@ Component data: {
 // look up by Ids
 export async function interface_users_ByIds(runtime: IAgentRuntime, ids: UUID[]): Promise<Record<UUID, any>> {
   //console.log('interface_users_ByIds', ids)
-  const entities = await runtime.getEntityByIds(ids)
+  const entities = await runtime.getEntitiesByIds(ids)
   //console.log('interface_users_ByIds - entities', entities, 'asked for', ids)
 
   // we should key this, each user can and should have only one COMPONENT_USER_TYPE component
@@ -58,7 +58,7 @@ export async function getUserIdsByPubkeys(runtime: IAgentRuntime, pubkeys): Prom
   const userIds = {} // revmap
   for(const entityId in emails) {
     const email = emails[entityId]
-    //console.log('getWalletByUserEntityIds_engine', entityId)
+    //console.log('getUserIdsByPubkeys', entityId)
     if (email.verified && email.address) {
       const emailEntityId = createUniqueUuid(runtime, email.address);
       //console.log('verified email.address', email.address, '=>', emailEntityId)
@@ -74,7 +74,7 @@ export async function getUserIdsByPubkeys(runtime: IAgentRuntime, pubkeys): Prom
     }
   }
 
-  const accounts = await runtime.getEntityByIds(Object.values(accountIds))
+  const accounts = await runtime.getEntitiesByIds(Object.values(accountIds))
   const list = {}
   for(const accountId in accounts) {
     const account = accounts[accountId]
@@ -104,7 +104,7 @@ export async function getUserIdsByPubkeys(runtime: IAgentRuntime, pubkeys): Prom
       // FIXME: remove from agent users if no component
       continue
     }
-    //console.log('getMetaWallets - ', entityId, 'wallets', email.metawallets)
+    //console.log('getUserIdsByPubkeys - ', entityId, 'wallets', email.metawallets)
     if (email.metawallets) {
       for(const mw of email.metawallets) {
         mws.push({...mw, entityId, names: email.names })
