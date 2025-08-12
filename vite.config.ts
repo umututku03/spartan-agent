8de4b72cd7b24ba129bc82787e074ac13984afb9
Odilitime <janesmith@airmail.cc>
2025-08-12 20:01:53 +0000

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import { defineConfig } from 'vitest/config';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/degen-intel/',
+  //base: '/degen-intel/',
+  base: './', // since we're injecting the base tag now (seems smarter than hardcoding the agent & plugin name)
+  //base: '/api/agents/Spartan/plugins/spartan-intel/',
   build: {
     emptyOutDir: false,
   },
